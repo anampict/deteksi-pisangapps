110b3db8e9d794391693bd4d5de7c7f411a7142a
anampict <anampict@gmail.com>
2026-04-13 21:27:29 +0700

    done

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -1,16 +1,33 @@
 import 'dart:convert';
 import 'dart:io';
 import 'package:http/http.dart' as http;
+import 'package:http_parser/http_parser.dart';
 import 'package:deteksipisang_app/config/app_config.dart';
 import 'package:deteksipisang_app/models/prediction_result.dart';
 
 class ApiService {
+  static MediaType _mimeType(String path) {
+    final ext = path.split('.').last.toLowerCase();
+    switch (ext) {
+      case 'png':
+        return MediaType('image', 'png');
+      case 'webp':
+        return MediaType('image', 'webp');
+      default:
+        return MediaType('image', 'jpeg');
+    }
+  }
+
   static Future<PredictionResult> predict(File imageFile) async {
     final uri = Uri.parse(AppConfig.predictUrl);
     final request = http.MultipartRequest('POST', uri);
 
     request.files.add(
-      await http.MultipartFile.fromPath('file', imageFile.path),
+      await http.MultipartFile.fromPath(
+        'file',
+        imageFile.path,
+        contentType: _mimeType(imageFile.path),
+      ),
     );
 
     final streamedResponse = await request.send();
